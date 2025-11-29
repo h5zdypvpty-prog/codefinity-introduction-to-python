4dd3345a9f32d094bc17015d7bce35771b18ba69
h5zdypvpty-prog <>
2025-11-29 03:23:21 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,10 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dai1 = grocery_items[0:5]
+dai2 = grocery_items[5:12]
+bake1 = grocery_items[12:18]
+fru1 = grocery_items[18:25]
+fru2 = grocery_items[25:33]
+meat1 = grocery_items[33:]
+item2 = dai1 + ", " + dai2 + ", and " + bake1 + " " + ("in aisle 5")
+
+print("we have dairy and bakery items:", item2)
